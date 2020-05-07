15dbc5459f033befc1390bb4727e9df8bc5b5707
pomeloyyyyyy <664719559@qq.com>
2020-05-07 14:31:49 +0800

    0507

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 // import {reqBaseCategoryList, reqLogin} from '@/api'
+import { reqFloors } from '@/api'
 
 export default {
   name: 'App',
@@ -25,9 +26,14 @@ export default {
     // const result2 = await reqLogin('13700000000', '111111')
     // console.log('result2', result2)
 
-    console.log(this.$store.state)
-    // 测试通过异步action获取异步获取数据到vuex的state中
-    // this.$store.dispatch('getBaseCategoryList')
+    // console.log(this.$store.state)
+
+    // 测试调用mock接口对应的接口请求函数
+    const result = await reqFloors()
+    console.log('mock result ', result)
+    
+    // 通过异步action获取异步获取数据到vuex的state中
+    this.$store.dispatch('getBaseCategoryList')
   },
 
   components: {
--- src/api/index.js
+++ src/api/index.js
@@ -3,17 +3,20 @@
 每个函数的返回值都是promise
 */
 
-import ajax from "./ajax";
+import ajax from './ajax'
+import mockAjax from './mockAjax'
 
 /* 
 请求获取3级分类列表
 /api/product/getBaseCategoryList
 */
 export function reqBaseCategoryList() {
+  // return ajax('/product/getBaseCategoryList')
   return ajax({
-    method: "GET",
-    url: "/product/getBaseCategoryList",
-  });
+    method: 'GET',
+    url: '/product/getBaseCategoryList'
+  })
+  // return ajax.get('/product/getBaseCategoryList')
 }
 
 /* 
@@ -21,12 +24,15 @@ export function reqBaseCategoryList() {
 /api/user/passport/login
 POST
 */
-export function reqLogin(mobile, password) {
+export function reqLogin (mobile, password) {
   return ajax({
-    method: "POST",
-    url: "/user/passport/login",
-    data: { mobile, password },
-  });
+    method: 'POST',
+    url: '/user/passport/login',
+    data: {mobile, password}
+  })
 
   // return ajax.post('/user/passport/login', {mobile, password})
 }
+
+export const reqBanners = () => mockAjax('/banners')
+export const reqFloors = () => mockAjax('/floors')
